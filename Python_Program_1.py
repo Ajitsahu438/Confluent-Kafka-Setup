e9d18089406edae454f3590aa04fc7878e5cf572
Ajitsahu438 <131419884+Ajitsahu438@users.noreply.github.com>
2023-04-28 08:41:40 +0530

    Update Python_Program_1.py

--- Python_Program_1.py
+++ Python_Program_1.py
@@ -1,3 +1,28 @@
 a=10
 b=9.2
 c="ajit"
+
+
+var1 = 123
+var2 = "123"
+
+print("Value of var1 : ",var1)
+print("Value of var2 : ",var2)
+
+print("Data Type of var1 : ",type(var1))
+print("Data Type of var2 : ",type(var2))
+
+var3 = int(var2) + 10
+print("Value of var3 : ",var3)
+print("Data Type of var3 : ",type(var3))
+
+
+var4 = 88.54
+var5 = int(var4)
+print("Value of var5 : ",var5)
+print("Data Type of var5 : ",type(var5))
+
+var6 = 90
+var7 = float(var6)
+print("Value of var7 : ",var7)
+print("Data Type of var7 : ",type(var7))
