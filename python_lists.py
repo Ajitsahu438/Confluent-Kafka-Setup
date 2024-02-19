6deb9c6b06f53098d6c25ed37886858b59f2c75f
Ajitsahu438 <131419884+Ajitsahu438@users.noreply.github.com>
2024-02-19 06:44:34 +0530

    Update python_lists.py

--- python_lists.py
+++ python_lists.py
@@ -1,7 +1,13 @@
-#arr list_2 = (1,2,3,4,5)
-#arr list_3 = (1,hi,3,hello,98,99)
+#arr_list_2 = (1,2,3,4,5)
+#arr_list_3 = (1,hi,3,hello,98,99)
 
-#print(arr list_2)
-#print(arr list_3)
+#print(arr_list_2)
+#print(arr_list_3)
 
+#arr_list =(1)
 
+#for i in range (1,11)
+
+#print(arr_list1)
+#list_len = len(arr_list1)
+#Print("Length of the list : ",list_len)
